b00b2834bc0c749477480a3e9d808bdd250d9bb7
Martin LEDRUT <46969020+sobix5@users.noreply.github.com>
2020-05-07 19:05:41 +0200

    bootstrap

--- script.js
+++ script.js
@@ -16,12 +16,16 @@ $(document).ready(function(){
             liveValue --;
         $("#valueText").text(liveValue);
         }
+        else if (liveValue <= 1) {
+            $("#minus").prop('disabled', true);
+        }
         
     });
 
     $("#plus").click(function(){
         liveValue ++;
         $("#valueText").text(liveValue);
+        $("#minus").prop('disabled', false);
     });
 
 
@@ -30,33 +34,54 @@ $(document).ready(function(){
         $("#valueText").text(liveValue);
     });
 
-// COLOR
-    $("#colorList").hide();
+    if(liveValue <= 0){
+        $("#minus").prop('disabled', true);
+    }
+    if(liveValue > 0){
+        $("#minus").prop('active', true);
+    }
 
-    $("#changeColorBut").click(function(){
-        $("#colorList").toggle();
-    });
+    //////////
+    // COLOR
+    //////////
 
-    $("#colorRed").click(function(){
+    $("#bgRed").click(function(){
         $("#show").css("background-color", "red");
       });
 
-    $("#colorGreen").click(function(){
+    $("#bgGreen").click(function(){
         $("#show").css("background-color", "green");
     });
 
-    $("#colorBlue").click(function(){
+    $("#bgBlue").click(function(){
         $("#show").css("background-color", "blue");
     });
 
-    $("#colorBlack").click(function(){
+    $("#bgBlack").click(function(){
         $("#show").css("background-color", "black");
     });
 
-    $("#colorWhite").click(function(){
+    $("#bgWhite").click(function(){
         $("#show").css("background-color", "white");
     });
+    //TEXT COLOR
+    $("#txtRed").click(function(){
+        $("#show h2, h1").css("color", "red");
+      });
 
+    $("#txtGreen").click(function(){
+        $("#show h2, h1").css("color", "green");
+    });
 
+    $("#txtBlue").click(function(){
+        $("#show h2, h1").css("color", "blue");
+    });
 
+    $("#txtBlack").click(function(){
+        $("#show h2, h1").css("color", "black");
+    });
+
+    $("#txtWhite").click(function(){
+        $("#show h2, h1").css("color", "white");
+    });
   });
